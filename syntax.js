d4719ea809bed846123a7067dd5cdc9fa2a4fac6
AKHIL-DyC <akhiljee666@gmail.com>
2023-06-24 20:04:01 +0530

    basic syntax of javascript

---
+++ syntax.js
@@ -0,0 +1,21 @@
+/*Syntax of an function 
+function functon_name(parameter1,parameter2){
+    code statement
+}
+parameters here are local variables
+
+
+syntax of a string
+let string_name ="akhil"
+or string_name ='akhil'
+or string_name = `akhil ${previously_defined_things}`temprol literate
+\r,\t,\' are called escape sequence
+
+syntax of object
+
+const object_name={
+    akhil:123
+    github:77
+}
+
+*/
