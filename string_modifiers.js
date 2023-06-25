f8d4e7af22c9fb2b64c9f5caa2af241e835f81e0
AKHIL-DyC <akhiljee666@gmail.com>
2023-06-25 14:05:39 +0530

    string modifiers

---
+++ string_modifiers.js
@@ -0,0 +1,17 @@
+let name="Akhil"
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.slice(1,3))
+let newname=name.replace("il","shay")
+console.log(newname)
+
+//trim() removes white spaces
+
+//for joining multiple string
+console.log(name.concat("is a friend of ",newname," keep that in mind"))
+console.log(name[2])//prints the 2 index of string similar to as of array 
+
+//to check if a  word is included or not
+const string="github is cool"
+const word ="github"
+console.log(string.includes(word))
