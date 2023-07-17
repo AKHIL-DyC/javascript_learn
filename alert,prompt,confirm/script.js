cceddb3e7a7665b70b7cbe60e588fa76d7474664
AKHIL-DyC <akhiljee666@gmail.com>
2023-07-17 11:35:10 +0530

    alert,prompt,confirm

---
+++ alert,prompt,confirm/script.js
@@ -0,0 +1,7 @@
+alert("learn javascript")
+let a = prompt("enter your favourite number to advance")
+a=Number.parseInt(a)
+console.log("type of a " +(typeof a))
+let write=confirm("do you want to continue")
+if(write){document.write("good choice")}
+else{document.write("bad choice")}
